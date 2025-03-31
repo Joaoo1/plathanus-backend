ebcb4a52291e4557990e659f85c0fa77264e238e
João <silva.joao1@outlook.com.br>
2025-03-30 22:44:16 -0300

    fix: update news validator not including params

--- src/modules/news/useCases/updateNews/UpdateNewsValidator.ts
+++ src/modules/news/useCases/updateNews/UpdateNewsValidator.ts
@@ -1,11 +1,15 @@
 import validate from 'express-zod-safe';
 import { z } from '../../../../common/libs/PtZod';
 
+const params = z.object({
+  id: z.string().uuid(),
+});
+
 const body = z.object({
   title: z.string().nonempty(),
   content: z.string().nonempty(),
 });
 
-export const UpdateNewsValidator = validate({ body });
+export const UpdateNewsValidator = validate({ body, params });
 
 export type UpdateNewsRequest = Parameters<typeof UpdateNewsValidator>[0];
