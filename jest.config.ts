6515b8d36b61817f1a07200c6d9d3e40be802ced
João <silva.joao1@outlook.com.br>
2025-03-31 03:25:26 -0300

    fix: ignore dist folder in Jest module resolution

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'jest';
 const config: Config = {
   coverageProvider: 'v8',
   preset: 'ts-jest',
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
 };
 
 export default config;
