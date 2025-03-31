764d414a09cb673b9037b0aafc8042565ff42767
João <silva.joao1@outlook.com.br>
2025-03-30 21:48:57 -0300

    feat: add UUID db extension and default value for user ID in migration

--- src/database/migrations/1716404308216-create-users-table.ts
+++ src/database/migrations/1716404308216-create-users-table.ts
@@ -3,9 +3,13 @@ import { type Kysely, sql } from 'kysely';
 import type { Database } from '../types';
 
 export async function up(db: Kysely<Database>): Promise<void> {
+  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`.execute(db);
+
   await db.schema
     .createTable('users')
-    .addColumn('id', 'uuid', col => col.primaryKey())
+    .addColumn('id', 'uuid', col =>
+      col.primaryKey().defaultTo(sql`uuid_generate_v4()`)
+    )
     .addColumn('name', 'varchar(255)', col => col.notNull())
     .addColumn('email', 'varchar(255)', col => col.notNull())
     .addColumn('passwordHash', 'varchar(255)', col => col.notNull())
