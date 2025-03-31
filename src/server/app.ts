66f6989e6ec299074c6f70e773b37131d7b71580
João <silva.joao1@outlook.com.br>
2025-03-30 23:23:08 -0300

    feat: add Swagger UI for API documentation

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './docs/swagger.json';
 import { ExceptionHandler } from './middlewares/ExceptionHandler';
 import { generalRateLimiter } from './middlewares/RateLimiter';
 import { router } from './router';
 
 const app = express();
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(cors({ origin: '*' }));
 
